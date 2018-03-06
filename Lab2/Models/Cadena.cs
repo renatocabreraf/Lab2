fbaf1f8296704f2cb431f52dac939e951aa9e132
renatocabreraf <renatojose98@yahoo.com>
2018-03-05 20:29:49 -0600

    Clase Pais

---
+++ Lab2/Models/Cadena.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab2.Models
+{
+    public class Cadena
+    {
+        public string cadena { get; set; }
+    }
+
+}
